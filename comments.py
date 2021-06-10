c863b895ef1faed4cd0f487e9c4d45d94a6076c9
Ruchitha G N <54787848+RuchithaRichii@users.noreply.github.com>
2021-06-10 14:36:56 +0530

    comments.py
    3. Write a program for a Single line comment, multi-line and documentation comments

---
+++ comments.py
@@ -0,0 +1,14 @@
+#printing a string
+print('Single line comment created')
+#it is a
+#multiline
+#comment
+'''
+I am a
+multiline comment
+'''
+print("Multi line comment created")
+def square(n):
+    '''Takes in a number n returns the square of n
+		type() method returns class type of the argument(object) passed as parameter. type() function is mostly used for debugging purposes. Two different types of arguments can be 			passed to type() function, single and three argument.'''
+    return n**2
